329fb2c872a5597947b0add70c01d7fca5daa750
TranQuocHuuSOS <0tranquochuu02003@gmail.com>
2023-07-18 17:49:07 +0700

    set up enviroment

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Nav from './components/NAV'
+function App() {
+ return (
+  <div className='grid grid-cols-5'>
+    <Nav></Nav>
+    <main className='col-span-4 bg-red-400'>
+      HHSHDHD
+    </main>
+  </div>
+ )
+}
+export default App
---
+++ src/components/NAV/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const index=()=>{
+    return(
+        <nav className='col-span-1 bg-cyan-200'>
+            <div className='flex mx-4 justify-between items-center'>
+                <h4></h4>
+            </div>
+        </nav>
+    )
+}
+export default index;
